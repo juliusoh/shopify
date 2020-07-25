d550cc94e053ffd1a0f4e361dd09c1f29ccd8b0c
julius oh <juliusoh@gmail.com>
2020-07-24 17:50:35 -0700

    user view product details front end

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Header from './header';
 import ProductList from './product-list';
+import ProductDetails from './product-details';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       message: null,
-      isLoading: true
+      isLoading: true,
+      view: {
+        name: 'catalog',
+        params: {}
+      }
     };
+    this.setView = this.setView.bind(this);
   }
 
   componentDidMount() {
@@ -19,11 +25,34 @@ export default class App extends React.Component {
       .finally(() => this.setState({ isLoading: false }));
   }
 
+  setView(name, params) {
+    const view = { name, params };
+    this.setState({ view });
+  }
+
+  userView() {
+    const { name, params } = this.state.view;
+    if (name === 'catalog') {
+      return (
+        <ProductList setView={this.setView} />
+      );
+    }
+    if (name === 'details') {
+      return (
+        <ProductDetails params={params} setView={this.setView} />
+      );
+    }
+    return null;
+  }
+
   render() {
+
     return (
       <div>
-        <Header />
-        <ProductList />
+        <Header title={'$Wicked Sales'}/>
+        <div className= "container-view">
+          {this.userView()}
+        </div>
       </div>
     );
   }
---
+++ client/components/product-details.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+export default class ProductDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: null
+    };
+  }
+
+  componentDidMount() {
+    this.retrieveDetails();
+  }
+
+  retrieveDetails() {
+    const productId = this.props.params.productId;
+    fetch(`/api/products/${productId}`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ product: data });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    const { product } = this.state;
+    const { setView } = this.props;
+    if (!product) {
+      return null;
+    }
+    return (
+      <div className="product-detail-container">
+        <div className="card">
+          <div className="card-header" onClick={() => setView('catalog', {})}><span><i className="fa fa-arrow-circle-left"></i></span>Back to Catalog</div>
+        </div>
+        <div className="card-body">
+          <div className="row">
+            <div className="product-image col">
+              <img src={product.image} />
+            </div>
+
+            <div className="short-description col">
+              <h3 className="card-title">{product.name}</h3>
+              <h4 className="price">${product.price}.00</h4>
+              <p className="description">{product.shortDescription}</p>
+            </div>
+          </div>
+
+          <div className="long-description">
+            <p className="card-text">{product.longDescription}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
